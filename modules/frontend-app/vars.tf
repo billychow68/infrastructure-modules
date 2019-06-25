dd43b265ad4d427d0d5d05b5be4bb1db33953a82
billychow68 <billchow07@gmail.com>
2019-06-25 08:40:25 -0700

    Add new module

---
+++ modules/frontend-app/vars.tf
@@ -0,0 +1,23 @@
+
+variable "min_size" {
+  description = "The minimum number of EC2 instances in the ASG"
+}
+variable "max_size" {
+  description = "The maximum number of EC2 instances in the ASG"
+}
+variable "region" {
+  default = "us-east-1"
+}
+variable "amis" {
+  type = map
+  default = {
+    "us-east-1" = "ami-b374d5a5"
+    "us-west-2" = "ami-4b32be2b"
+  }
+}
+variable "ssh_port" {
+  default = "22"
+}
+variable "server_port" {
+  default = "8080"
+}
